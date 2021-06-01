d5b791461125c0e1644b9fec9f393aad5a873230
Damaris Uchuaia <80401389+damaris-uchuaia@users.noreply.github.com>
2021-06-01 15:19:13 -0300

    first commit

---
+++ src/app/first/first.components.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+@Component({
+ selector:'first',
+ templateUrl:'./first.component.html',
+ styleUrls: ['./first.component.css'],
+})
+export class FirstComponent{
+
+}
---
+++ src/app/second/second.component.ts
@@ -0,0 +1,16 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'second',
+  templateUrl: './second.component.html',
+  styleUrls: ['./second.component.css']
+})
+export class SecondComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+    console.log('esse componente foi renderizado completamente')
+  }
+
+}
